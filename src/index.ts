877e13d5d57b1ba969e3b483e2d44092072debbf
monyone <monyone.teihen@gmail.com>
2024-07-21 19:44:40 +0900

    Fix: Export Issue

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-export { default as EOFError} from './util/eof'
+export { default as EOFError} from './util/eof';
 
-export type { TimestampedSegment as TimestampedPGSSegment } from './pgs/type';
+export type * from './pgs/type'
+export * from './pgs/type';
 
 export { default as readPGSFromSup } from './pgs/sup';
 export { default as collectDisplaySet } from './pgs/displayset';
 export { default as convertAcquisitionPoint } from './pgs/acquisitionpoint';
 
-export type { default as PGSFeeder } from './runtime/common/feeder'
-export { default as PGSSupFeeder } from './runtime/common/supfeeder'
+export type { default as PGSFeeder } from './runtime/common/feeder';
+export { default as PGSSupFeeder } from './runtime/common/supfeeder';
 
-export { default as PGSController } from './runtime/browser/controller'
-
+export { default as PGSController } from './runtime/browser/controller';
+export { default as PGSRenderer } from './runtime/browser/renderer';
