07303316e7a8434e2f9bacb1224258191b3782a4
monyone <monyone.teihen@gmail.com>
2024-07-24 07:48:44 +0900

    Fix: Make abstract class to PGSRenderer

--- src/runtime/browser/renderer-main.ts
+++ src/runtime/browser/renderer-main.ts
@@ -3,40 +3,13 @@ import { AcquisitionPoint } from "../../pgs/type";
 import PGSRenderer from "./renderer";
 import decode from "./decoder";
 
-export default class PGSMainThraedRenderer<T extends HTMLCanvasElement | OffscreenCanvas> implements PGSRenderer<T> {
-  private canvas: T | null = null;
-
-  public attach(canvas: T): void {
-    this.canvas = canvas;
-  }
-
-  public snapshot(): T | null {
-    return this.canvas;
-  }
-
-  public remove(): void {
-    if (this.canvas == null) { return; }
-    if (!(this.canvas instanceof HTMLCanvasElement)) { return; }
-
-    this.canvas.remove();
-  }
-
-  public detach(): void {
-    this.canvas = null;
-  }
-
-  public resize(width: number, height: number): void {
-    if (this.canvas == null) { return; }
-
-    this.canvas.width = width;
-    this.canvas.height = height;
-  }
+export default class PGSMainThraedRenderer<T extends HTMLCanvasElement | OffscreenCanvas> extends PGSRenderer<T> {
 
   public render(pgs: Readonly<AcquisitionPoint>): void {
     if (this.canvas == null) { return; }
 
     // Omit False Positive ImageBitmapRenderingContext type
-    const context = this.canvas.getContext('2d') as CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D | null;
+    const context = this.getContext2D();
     if (!context) { return; }
     context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
@@ -46,15 +19,5 @@ export default class PGSMainThraedRenderer<T extends HTMLCanvasElement | Offscre
     context.drawImage(source, 0, 0, this.canvas.width, this.canvas.height);
 
     source.width = source.height = 0;
-  }
-
-  public clear(): void {
-    if (this.canvas == null) { return; }
-
-    // Omit False Positive ImageBitmapRenderingContext type
-    const context = this.canvas.getContext('2d') as CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D | null;
-    if (!context) { return; }
-
-    context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
 }
--- src/runtime/browser/renderer.ts
+++ src/runtime/browser/renderer.ts
@@ -1,11 +1,49 @@
 import { AcquisitionPoint } from "../../pgs/type";
 
-export default interface PGSRenderer<T extends HTMLCanvasElement | OffscreenCanvas> {
-  attach(canvas: T): void;
-  snapshot(): T | null;
-  remove(): void;
-  detach(): void;
-  resize(width: number, height: number): void;
-  render(pgs: Readonly<AcquisitionPoint>): void;
-  clear(): void;
+export default abstract class PGSRenderer<T extends HTMLCanvasElement | OffscreenCanvas> {
+  protected canvas: T | null = null;
+
+  public attach(canvas: T): void {
+    this.canvas = canvas;
+  }
+
+  public snapshot(): T | null {
+    return this.canvas;
+  }
+
+  public remove(): void {
+    if (this.canvas == null) { return; }
+    if (!(this.canvas instanceof HTMLCanvasElement)) { return; }
+
+    this.canvas.remove();
+  }
+
+  public detach(): void {
+    this.canvas = null;
+  }
+
+  public resize(width: number, height: number): void {
+    if (this.canvas == null) { return; }
+
+    this.canvas.width = width;
+    this.canvas.height = height;
+  }
+
+  protected getContext2D(): CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D | null {
+    if (this.canvas == null) { return null; }
+
+    // Omit False Positive ImageBitmapRenderingContext type
+    return this.canvas.getContext('2d') as CanvasRenderingContext2D | OffscreenCanvasRenderingContext2D | null;
+  }
+
+  public clear(): void {
+    if (this.canvas == null) { return; }
+
+    const context = this.getContext2D();
+    if (!context) { return; }
+
+    context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  public abstract render(pgs: Readonly<AcquisitionPoint>): void;
 }
